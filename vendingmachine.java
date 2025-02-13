151d8d547c67c1ac008aa49dfce7f29d7a57aef5
SmSeeley <smseeley@quinnipiac.edu>
2025-02-12 19:27:17 -0500

    added scanner to get user input and displayed a welcome message

--- vendingmachine.java
+++ vendingmachine.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class vendingmachine {
+
     public static void main(String[] args){
+
+        //Create scanner
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("1. Oreos: $2.00 \n2. Chips: $1.00\n3. Coca-cola: $2.50\n4. Pepsi: $1.00\n5. Water: $2.50");
+        System.out.println("\nWelcome to the vending machine please pick a snack to buy: ");
+
+        String choice = scanner.nextLine();
+
     }
 }
