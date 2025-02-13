f869da3471ab74fed60e8d8930756c3837fbc762
LucasJenkins731 <lgjenkins@quinnipiac.edu>
2025-02-12 19:12:28 -0500

    Added snacks and prices

---
+++ vendingmachine.java
@@ -0,0 +1,5 @@
+public class vendingmachine {
+    public static void main(String[] args){
+        System.out.println("1. Oreos: $2.00 \n2. Chips: $1.00\n3. Coca-cola: $2.50\n4. Pepsi: $1.00\n5. Water: $2.50");
+    }
+}
